da437e66a173efe3fa39b191dea0b38b821006d6
Denis <ddujota@internationalsafety.com>
2019-04-17 13:30:58 -0400

    finished the reducers and compibned reducers method

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,28 @@
+// import the combine reducers so we cna create a state object from all the reducer functions
+import { combineReducers } from 'redux';
+
 const songsReducer = () => [
-  { title: 'Zoom Zoom', artist: 'Polo & Pan' },
-  { title: 'Trauma', artist: "N'TO" },
-  { title: 'Salzburg', artist: 'Worakls' },
-  { title: 'Hortari', artist: 'Worakls' },
+  { title: 'Zoom Zoom', artist: 'Polo & Pan', duration: '4:05' },
+  { title: 'Trauma', artist: "N'TO", duration: '7:02' },
+  { title: 'Salzburg', artist: 'Worakls', duration: '8:45' },
+  { title: 'Hortari', artist: 'Worakls', duration: '6:16' },
 ];
+
+// default null becuase when the app first fires up it should be with no selected song
+const selectedSongReducer = (selectedSong = null, action) => {
+  const { type, payload } = action;
+
+  // this allows us to expand our code with future actions and types
+  if (type === 'SELECTED_SONG') {
+    return payload;
+  }
+
+  return selectedSong;
+};
+
+// finally export defualt here so that all the files can have access to combined reducers method and the code herein
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
