01338507af02222f8337dbf2fc11cab8cf5fde49
Denis <ddujota@internationalsafety.com>
2019-04-17 14:43:58 -0400

    adde din the proptpes and the song list rendering

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class SongList extends React.Component {
+  static propTypes = {
+    songs: PropTypes.array,
+  };
+
   render() {
+    // Console logging the props object wlll show the songs arary (state) and also the dispatch(action) fucntion is referenced in props
     console.log(this.props);
-    return <div>SongList</div>;
+
+    const { songs } = this.props;
+    return (
+      <div>
+        {songs.map(song => (
+          <div className="item" key={song.title}>
+            <div className="right floated content">
+              <button type="button" className="ui button primary">
+                Select
+              </button>
+            </div>
+            <div className="content">{song.title}</div>
+          </div>
+        ))}
+        ;
+      </div>
+    );
   }
 }
 /* By convention we call this mapStateToProps, this is required in order to get the state from redux into our component from the connect function -- data from redux will show up as props in our component
