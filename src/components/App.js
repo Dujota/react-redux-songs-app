91e4e0832c315f74e8f9ae02dae5a0dfadb44370
Denis <ddujota@internationalsafety.com>
2019-04-17 13:23:11 -0400

    added a reducer that is a static list of songs

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { selectSong } from '../actions';
 
 // All state will be on the redux store so we can have a simple app component
 const App = () => <div>App</div>;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+const songsReducer = () => [
+  { title: 'Zoom Zoom', artist: 'Polo & Pan' },
+  { title: 'Trauma', artist: "N'TO" },
+  { title: 'Salzburg', artist: 'Worakls' },
+  { title: 'Hortari', artist: 'Worakls' },
+];
